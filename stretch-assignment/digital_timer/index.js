56c172477d967214e5ece723cb74278de3d5cd5f
eralpkor <eralp.ny@gmail.com>
2019-08-05 18:37:28 -0400

    timer stretch goal done

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,17 +3,24 @@ console.log('working');
 let secondsTens = document.getElementById('secondTens');
 let secondsOnes = document.getElementById('secondOnes');
 let msHundreds = document.getElementById('msHundreds');
-let msTens = document.getElementById('meTens');
+let msTens = document.getElementById('msTens');
 
 
 secondsTens.innerText = '0';
 secondsOnes.innerText = '0';
+msHundreds.innerText = '0';
 
 (function timer() {
   let miliSeconds = 0;
   var count = setInterval(() => {
-    msHundreds.innerText = miliSeconds++;
-
+    msTens.innerText = miliSeconds++;
+    if (miliSeconds === 10) {
+      msHundreds.innerText = 1;
+      msTens.innerText = 0;
+    } 
     if (miliSeconds === 10) clearInterval(count)
+    // msHundreds.innerText = 1;
   }, 100);
+
+  
 })();
